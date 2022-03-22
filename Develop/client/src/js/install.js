82e38fafd7d4f3a6ebed0ed33da92545deec13da
ramriddlez <ramankangz@outlook.com>
2022-03-21 20:58:20 -0400

    added install bug fix

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,20 +2,39 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
+// window.addEventListener('beforeinstallprompt', (event) => {
+//     event.preventDefault();
+//     butInstall.style.visibility = 'visible';
+
+//     butInstall.addEventListener('click', async () => {
+//         event.prompt();
+//         butInstall.setAttribute('disabled', true);
+        
+//     });
+// });
+
+// // TODO: Add an handler for the `appinstalled` event
+// window.addEventListener('appinstalled', (event) => {
+
+//     console.log('👍', 'appinstalled', event)
+// });
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
-    // butInstall.classList.toggle('hidden', false)
-    butInstall.style.visibility = 'visible';
+    window.deferredPrompt = event
+    butInstall.classList.toggle('hidden', false) 
 });
-butInstall.addEventListener('click', async (event) => {
-    event.prompt();
-    butInstall.setAttribute('disabled', true);
-
-});
-
+    butInstall.addEventListener('click', async () => {
+        const promptEvent = window.deferredPrompt
+        if (!promptEvent) {
+            return
+        } 
+        promptEvent.prompt();
+        butInstall.classList.toggle('hidden', true) 
+        
+    });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-
+    window.deferredPrompt = null
     console.log('👍', 'appinstalled', event)
 });
